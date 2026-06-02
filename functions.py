dd9a0a0b104e3820f9f57abe551ceb0b151fb034
lokeshwar567 <earlalokeshwar@gmail.com>
2026-06-02 16:14:49 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+from operator import length_hint
+
+
+def say_hello():
+    print("welcome to python")
+say_hello()
+def area_r(length,width):
+    print(length*width)
+area_r(6,4)
+def mul(a,b,c):
+    print(a*b*c)
+mul(1,2,3)
+def na_me(animal,name):
+    print(f"My {animal} is named {name}")
+na_me('cat','gut')
+def power(base,ex=2):
+    print(base**ex)
+power(3)
